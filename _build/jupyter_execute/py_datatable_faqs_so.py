43839fe03f26133a0cbb791f3d7bc01ab7823d07
myamullacienica <myamulla.cienica@gmail.com>
2020-09-06 11:58:51 +0000

    Stackoverflow 10 questions prepared

--- _build/jupyter_execute/py_datatable_faqs_so.py
+++ _build/jupyter_execute/py_datatable_faqs_so.py
@@ -162,7 +162,7 @@ sales_DT
 
 That is OK, you have noticed or tried converting a column from string to any other types **(int,float,bool)**
 
-#sales_DT['profit_perc'] = float
+sales_DT['profit_perc'] = float
 
 **Note** : String to other type converions are not yet implemented in datatable versions till 0.10.1 and they would be surely implemented in the upcoming versions.
 
@@ -221,4 +221,113 @@ Here we have given a column names in a list and let us understand the anotomy of
 What if our requirement is based on a type of column, of course it can also be achived with the same syntax as follows. and here we are deselecting all the string type columns from spotify dataframe.
 
 spotify_songs_dt[:,f[:].remove(f[dt.str32])]
+
+### 1.6 How to remove a list of columns from dataframe?
+
+We already learnt how to deselect a list of columns, but here we are now going to see how to remove a list of columns from dataframe. and the below is the sytax for it.
+
+            del DT[:,columns to be removed]
+
+Let us create a sample dataframe as - 
+
+comidas_gen_dt = dt.Frame({
+    'country':list('ABCDE'),
+    'id':[1,2,3,4,5],
+    'egg':[10,20,30,5,40],
+    'veg':[30,40,10,3,5],
+    'fork':[5,10,2,1,9],
+    'beef':[90,50,20,None,4]})
+
+comidas_gen_dt
+
+I would like to remove a column id from this dataframe following the above syntax - 
+
+del comidas_gen_dt[:,"id"]
+
+comidas_gen_dt
+
+we noticed that the column id is no more existed in comidas dataframe, and if we want to remove more than one column their names should be passed in a list type as below-
+
+del comidas_gen_dt[:,["veg","beef"]]
+
+comidas_gen_dt
+
+We are just left with 3 columns and i'm making a copy of this dataframe as - 
+
+DT_comidas = comidas_gen_dt.copy()
+
+DT_comidas is an independent from the comidas_gen_dt dataframe, means whatever the changes we make on comidas_get_dt those will not be replicated on DT_comidas and let us see how it is worked out.
+
+From dataframe comidas_gen_gt we are going to remove a column fork as - 
+
+del comidas_gen_dt[:,"fork"]
+
+comidas_gen_dt
+
+DT_comidas
+
+Fork column is still available in DT_comidas and it is removed from comidas_gen_dt.
+
+### 1.7 How to filter NA's from dataframe ?
+
+datos = """
+       id|charges|payment_method
+       634-VHG|28|Cheque
+       365-DQC|33.5|Credit card
+       264-PPR|631|--
+       845-AJO|42.3|--
+       769-LPO|56.9|Gpay
+       529-GPO|56.9|--
+       903-QPO|--|--
+       234-CPO|89.9|Bank Transfer
+       102-PPO|26.9|Cash
+       303-LPO|16.9|--
+       692-CPO|--|PhonePay
+       """
+
+payments_dt = dt.fread(datos,na_strings=["--"])
+
+We will have a sample dataframe for this case as below.
+
+payments_dt
+
+We have a function called **countna** from datatable, on applying it on dataframe it will count how many of NA's are in each column and returns a dataframe as below.
+
+payments_dt.countna()
+
+Here we have 2 NA's in charges column and 5 NA's in payment method.
+
+As per the Datatable sytax we have a filter option I position as,
+
+                           DT[I,:]
+
+We can now call a function called **isna** from datatable, pass a column name whose NA's should be checked. For example we would like to filter NAs for a column charges as follows - 
+
+payments_dt[dt.isna(f.charges),:]
+
+What if we would like to not to include NA's observations per a column charges, and it can be done as follows- here we have just append that isna function with a symbol (~).
+
+payments_dt[~dt.isna(f.charges),:]
+
+We could also use the **isna** in by position to count how many of observations are having NA's per in specified column.
+
+payments_dt[:,count(),by(dt.isna(f.payment_method))]
+
+We have observed that there are 5 NA's and 6 observations are having the values.
+
+There are some cases where we have to see two or more observations are together having the NA's. for example we can check for NA's across payment method and charges as below. we have made use of some logical operations and will be discussed when a case comes in. for now you can just remember this syntax.
+
+payments_dt[:,count(),by((dt.isna(f.payment_method)) & (dt.isna(f.charges)))]
+
+Here we can see that there is only one observation having NA's for both of these columns.
+
+### 1.8 How to modify or update column values on a condition?
+
+payments_dt[dt.isna(f.charges),f.charges]=20.45
+
+payments_dt
+
+payments_dt[f.id=="102-PPO",f.id]="103-PPO"
+
+payments_dt
 
